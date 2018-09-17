fdd4ffd9272f1fc472c062b577c22157ab318405
Sukhmeet Singh <sukhmeet2390@gmail.com>
2018-09-17 15:19:49 +0530

    Add new exception - CarAlreadyInUse * To be thrown when car is not available for selection

--- src/main/java/com/mytaxi/exception/EntityNotFoundException.java
+++ src/main/java/com/mytaxi/exception/EntityNotFoundException.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class EntityNotFoundException extends Exception {
     static final long serialVersionUID = -3387516993334229948L;
 
-
     public EntityNotFoundException(String message) {
         super(message);
     }
