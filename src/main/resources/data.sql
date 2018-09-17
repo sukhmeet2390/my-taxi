52069207191ebfb758ce6bf7e7dc7334abe00a56
Sukhmeet Singh <sukhmeet2390@gmail.com>
2018-09-17 13:10:04 +0530

    Add cars data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,39 +4,53 @@
 
 -- Create 3 OFFLINE drivers
 
-insert into driver (id, date_created, deleted, online_status, password, username) values (1, now(), false, 'OFFLINE',
-'driver01pw', 'driver01');
+INSERT INTO driver (id, date_created, deleted, online_status, password, username) VALUES (1, now(), FALSE, 'OFFLINE',
+                                                                                          'driver01pw', 'driver01');
 
-insert into driver (id, date_created, deleted, online_status, password, username) values (2, now(), false, 'OFFLINE',
-'driver02pw', 'driver02');
+INSERT INTO driver (id, date_created, deleted, online_status, password, username) VALUES (2, now(), FALSE, 'OFFLINE',
+                                                                                          'driver02pw', 'driver02');
 
-insert into driver (id, date_created, deleted, online_status, password, username) values (3, now(), false, 'OFFLINE',
-'driver03pw', 'driver03');
-
+INSERT INTO driver (id, date_created, deleted, online_status, password, username) VALUES (3, now(), FALSE, 'OFFLINE',
+                                                                                          'driver03pw', 'driver03');
 
 -- Create 3 ONLINE drivers
 
-insert into driver (id, date_created, deleted, online_status, password, username) values (4, now(), false, 'ONLINE',
-'driver04pw', 'driver04');
+INSERT INTO driver (id, date_created, deleted, online_status, password, username) VALUES (4, now(), FALSE, 'ONLINE',
+                                                                                          'driver04pw', 'driver04');
 
-insert into driver (id, date_created, deleted, online_status, password, username) values (5, now(), false, 'ONLINE',
-'driver05pw', 'driver05');
+INSERT INTO driver (id, date_created, deleted, online_status, password, username) VALUES (5, now(), FALSE, 'ONLINE',
+                                                                                          'driver05pw', 'driver05');
 
-insert into driver (id, date_created, deleted, online_status, password, username) values (6, now(), false, 'ONLINE',
-'driver06pw', 'driver06');
+INSERT INTO driver (id, date_created, deleted, online_status, password, username) VALUES (6, now(), FALSE, 'ONLINE',
+                                                                                          'driver06pw', 'driver06');
 
 -- Create 1 OFFLINE driver with coordinate(longitude=9.5&latitude=55.954)
 
-insert into driver (id, coordinate, date_coordinate_updated, date_created, deleted, online_status, password, username)
-values
- (7,
- 'aced0005737200226f72672e737072696e676672616d65776f726b2e646174612e67656f2e506f696e7431b9e90ef11a4006020002440001784400017978704023000000000000404bfa1cac083127', now(), now(), false, 'OFFLINE',
-'driver07pw', 'driver07');
+INSERT INTO driver (id, coordinate, date_coordinate_updated, date_created, deleted, online_status, password, username)
+VALUES
+  (7,
+   'aced0005737200226f72672e737072696e676672616d65776f726b2e646174612e67656f2e506f696e7431b9e90ef11a4006020002440001784400017978704023000000000000404bfa1cac083127',
+   now(), now(), FALSE, 'OFFLINE',
+   'driver07pw', 'driver07');
 
 -- Create 1 ONLINE driver with coordinate(longitude=9.5&latitude=55.954)
 
-insert into driver (id, coordinate, date_coordinate_updated, date_created, deleted, online_status, password, username)
-values
- (8,
- 'aced0005737200226f72672e737072696e676672616d65776f726b2e646174612e67656f2e506f696e7431b9e90ef11a4006020002440001784400017978704023000000000000404bfa1cac083127', now(), now(), false, 'ONLINE',
-'driver08pw', 'driver08');
+INSERT INTO driver (id, coordinate, date_coordinate_updated, date_created, deleted, online_status, password, username)
+VALUES
+  (8,
+   'aced0005737200226f72672e737072696e676672616d65776f726b2e646174612e67656f2e506f696e7431b9e90ef11a4006020002440001784400017978704023000000000000404bfa1cac083127',
+   now(), now(), FALSE, 'ONLINE',
+   'driver08pw', 'driver08');
+
+-- Create 4 cars
+INSERT INTO car (id, date_created, license_plate, seat_count, convertible, rating, engine_type, manufacturer, deleted, color)
+VALUES (1, now(), 'License_ABC', 4, FALSE, 5.0, 'GAS', 'Honda', FALSE, 'blue');
+
+INSERT INTO car (id, date_created, license_plate, seat_count, convertible, rating, engine_type, manufacturer, deleted, color)
+VALUES (2, now(), 'License_DEF', 4, FALSE, 2.0, 'GAS', 'Toyota', FALSE, 'white');
+
+INSERT INTO car (id, date_created, license_plate, seat_count, convertible, rating, engine_type, manufacturer, deleted, color)
+VALUES (3, now(), 'License_GHI', 6, FALSE, 3.5, 'GAS', 'Suzuki', FALSE, 'black');
+
+INSERT INTO car (id, date_created, license_plate, seat_count, convertible, rating, engine_type, manufacturer, deleted, color)
+VALUES (4, now(), 'License_JKL', 2, FALSE, 4.0, 'ELECTRIC', 'Tesla', FALSE, 'grey');
